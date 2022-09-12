815713d5e31a9a13caed40d8a23b4117ffedb271
Your Name <you@example.com>
2022-09-11 18:38:45 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,21 +1,37 @@
-#include <stdio.h>                                                                                                        
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: print
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n1 = 0, n2;
 
-#include <stdlib.h>                                                                                                       
+	while (n1 <= 9)
+	{
+		n2 = 0;
+		while (n <= 9)
+		{
+			if (n1 != n2 && n1 < n2)
+		{
+			putchar(n1 + 48);
+			putchar(n2 + 48);
 
-/**                                                                                                                       
- *
- *  *                                                                                                                        
- *
- *   *                                                                                                                        
- *
- *    * Description: print                                                                                                     
- *
- *     *                                                                                                                        
- *
- *      *                                                                                                                        
- *
- *       *                                                                                                                        
- *
- *        */                                                                                                                       
+			if (n1 + n2 != 17)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		++n2;
+	}
+	++n1;
+	}
+	putchar('\n');
+	return (0);
+}
 
-int main()                                                                                                        
