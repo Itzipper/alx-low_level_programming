9f7f06bbc39020ad39cee5936312ff29f7750822
Your Name <you@example.com>
2022-09-13 14:12:41 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,7 +12,7 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (N == 0)
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
@@ -20,6 +20,6 @@ int print_sign(int n)
 	else
 	{
 		_putchar('-');
-		return (-);
+		return (-1);
 	}
 }
