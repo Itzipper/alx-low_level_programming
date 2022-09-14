06376de1b0c5ec2ad25a0687436f43fa24344a55
Your Name <you@example.com>
2022-09-14 13:51:59 -0700

    Task 12

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,13 +6,13 @@
  * Return: Always 0 (Success)
  *
  */
-int main(void)
+_int main(void)
 {
 	unsigned long int sum3, sum5, sum
 		int 1;
 	sum3 = 0;
 	sum5 = 0;
-	sum = 0;
+
 	for (i = 0; i < 1024; ++i)
 	{
 		if ((i % 3) == 0)
