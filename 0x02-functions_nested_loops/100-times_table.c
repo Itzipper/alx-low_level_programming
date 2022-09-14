11e2b15c876947a8a7f060ed080d6c7fef570d7c
Your Name <you@example.com>
2022-09-14 15:40:17 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,7 +2,7 @@
 /**
  * print_times_table - prits time table of n
  *
- * @int: integer to put in this table
+ * @n: integer to put in this table
  */
 
 void print_times_table(int n)
@@ -13,26 +13,26 @@ void print_times_table(int n)
 	{
 		for (num = 0; num <= n; num++)
 		{
-			_putchar(48);
+			putchar(48);
 			for (mult = 1; mult <= n; mult++)
 			{
-				_putchar(',');
-				_putchar(' ');
+				putchar(',');
+				putchar(' ');
 				prod = num * mult;
 				if (prod <= 9)
-					_putchar(' ');
+					putchar(' ');
 				if (prod <= 99)
-					_putchar(' ');
+					putchar(' ');
 				if (prod >= 100)
 				{
-					_putchar((prod / 100) + 48);
-					_putchar((prod / 10) % 10 + 48);
+					putchar((prod / 100) + 48);
+					putchar((prod / 10) % 10 + 48);
 				}
 				else if (prod <= 99 && prod >= 10)
-					_putchar((prod / 10) + 48);
-				_putchar((prod % 10) + 48);
+					putchar((prod / 10) + 48);
+				putchar((prod % 10) + 48);
 				}
-			_putchar('\n');
+			putchar('\n');
 		}
 	}
 }
