b880c2a19f8caf11a1c736cecf2187ddedff397f
Your Name <you@example.com>
2022-09-13 17:12:07 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * times - function that prints tge 9 times table, starting with 0
+ * rw = row, col = columon, d = digits
+ * Return: Always success
+ */
+void times_table (void)
+{
+	int rw, col, d;
+	for (rw = 0; rw <= 9; rw++)
+	{
+		_putchar('0');
+		_putchar(',');
+	        _putchar(' ');
+	for (col = 1; col <= o; col++)
+	{
+	d = (rw * col);
+
+if ((d / 10) > 0)
+{
+
+_putchar ((d / 10) + '0');
+}
+
+else
+{
+_putchar(' ');
+}
+",,,5C
+
