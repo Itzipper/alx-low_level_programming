527f79b54b1a0d5997d642d49aa7c9a66faecded
Your Name <you@example.com>
2022-09-15 15:26:23 -0700

    Task 12

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_traingle - prints a triangle
+ * @size: size parameterof triangle
+ * Return: returns nothing
+ */
+
+void print_triangle(int size)
+{
+	int inc1, inc2;
+
+		if (size > 0)
+		{
+			for (inc1 = 1; inc1 <= size; inc1++)
+			{
+			for ((inc2 = size - inc1); inc2 > 0; inc2--)
+			{
+				putchar(' ');
+			}
+
+			for (inc2 = 0; inc1; inc2++)
+			{
+				putchar('#');
+			}
+
+			if (inc1 == size)
+			{
+				continue;
+			}
+
+			putchar('\n');
+		}
+		}
+}
