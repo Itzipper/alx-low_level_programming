42581000b41f8de252748c5635ebd518e549a068
Your Name <you@example.com>
2022-09-16 15:40:04 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,21 +14,13 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i == 100)
-		{
 			printf("Buzz");
-		}
 		else if (((i % 3) == 0) && ((i % 5) == 0))
-		{
 			printf("FizzBuzz ");
-		}
 		else if ((i % 3) == 0)
-		{
 			printf("Fuzz ");
-			}
 		else if ((i % 5) == 0)
-		{
 			printf("Buzz ");
-		}
 		else
 			printf("%d ", i);
 	}
