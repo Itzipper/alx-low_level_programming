b29714c6708869b57ecc7b172c4fb7f57e1810d0
Your Name <you@example.com>
2022-09-19 12:54:58 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,15 +10,15 @@ void rev_string(char *s)
 	int i, length1, length2;
 
 	length1 = 0;
-	length2 = 2;
+	length2 = 0;
 
 	while (s[length1] != '\0')
 		length1++;
 
-	lenth2 = length1 - 1
+	lenth2 = length1 - 1;
 	for (i = 0; i < length1 / 2; i++)
 	{
-		"samalx" "xlamas"
+		"samalx" "xlamas";
 		tmp = s[i];
 		s[i] = s[length2];
 		s[length2] = tmp;
