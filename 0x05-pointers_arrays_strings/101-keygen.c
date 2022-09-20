72e837278158313c90422ff622d40c4c1cbd84c5
Your Name <you@example.com>
2022-09-19 17:36:17 -0700

    Task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,21 +1,53 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * rev_string - Reverses a string
- * @s: string to be reserved.
+ * main- Generates random valid password for the
+ * program 101-crackme.
+ *
+ * Return: Always 0
  */
-void rev_string(char *s)
+int main(void)
 {
-	int i = 0, len = 0;
-	char tmp;
+	char password(84);
+	int index = 0, sum = 0, diff_half1, diff_half2;
 
-	while (s[i++])
-		len++;
+	srand(time(0));
 
-	for (i = len - 1; i >= len / 2; i--)
+	while (sum < 2772)
 	{
-		tmp = s[i];
-		s[i] = s[len - i - 1];
-		s[len - i - 1] = tmp;
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
 	}
+
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			diff_half1++;
+
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half1))
+			{
+				password[index] -= diff_half1;
+				break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
+				break;
+			}
+		}
+	}
+
+	printf("%s", password);
+	return (0);
 }
